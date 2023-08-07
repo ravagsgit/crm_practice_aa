f21365e4930c5e6488b5868e1f9f99ffdf25534b
ravshan.sharifov <sharifovravshan5@gmail.com>
2023-08-07 10:34:36 +0500

    adding ClientService class

---
+++ src/Crm/ClientService.cs
@@ -0,0 +1,25 @@
+using Crm;
+
+public sealed class ClientService
+{
+    public Client CreateClient(
+        string firstName,
+        string lastName,
+        string middleName,
+        short age,
+        string passportNumber,
+        string gender
+    )
+    {
+        // TODO: Validate input parameters.
+        return new()
+        {
+            FirstName = firstName,
+            LastName = lastName,
+            MiddleName = middleName,
+            Age = age,
+            PassportNumber = passportNumber,
+            Gender = gender
+        };
+    }   
+}
