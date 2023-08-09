006fcdde8bc439ed48e624283c18c9b7b8fb741e
ravshan.sharifov <sharifovravshan5@gmail.com>
2023-08-09 15:11:12 +0500

    _

--- src/Crm/Services/ClientService.cs
+++ src/Crm/Services/ClientService.cs
@@ -4,17 +4,24 @@ using Crm.Entities.DTOs;
 namespace Crm.Services;
 public sealed class ClientService
 {
+    private List<Client> clients = new List<Client>();
     public Client CreateClient(ClientInfo clientInfo)
     {
-        // TODO: Validate input parameters.
-        return new()
+        Client newClient =  new()
         {
             FirstName = clientInfo.FirstName,
             LastName = clientInfo.LastName,
             MiddleName = clientInfo.MiddleName,
             Age = clientInfo.Age,
             PassportNumber = clientInfo.PassportNumber,
-            Gender = clientInfo.ClientGender
+            Gender = clientInfo.ClientGender,
+            Phone = clientInfo.Phone,
+            Email = clientInfo.Email,
+            Password = clientInfo.Phone
         };
+
+        clients.Add(newClient);
+        Console.WriteLine("New client was created successiful and added to list.");
+        return newClient;
     }   
 }
