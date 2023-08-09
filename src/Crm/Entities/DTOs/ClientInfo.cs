709e764c9b1a2fbe0b2b7009ab9d460dc1098447
ravshan.sharifov <sharifovravshan5@gmail.com>
2023-08-09 08:45:42 +0500

    Added first DOTs object - ClientInfo.cs

---
+++ src/Crm/Entities/DTOs/ClientInfo.cs
@@ -0,0 +1,11 @@
+﻿namespace Crm.Entities.DTOs;
+
+public readonly struct ClientInfo
+{
+    public string FirstName { get; init; }
+    public string LastName { get; init; }
+    public string? MiddleName { get; init; }
+    public short Age { get; init; }
+    public string PassportNumber { get; init; }
+    public Gender Gender { get; init; }
+}
